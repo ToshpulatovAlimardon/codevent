0513d2641cd0fb5da60bcc6df894b2e7e59006eb
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-08-17 17:20:58 +0500

    changed countdown 24hours

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
-const startingMinutes = 60;
-let time = startingMinutes * 60;
+const startingMinutes = 24;
+let time = startingMinutes * 3600;
 
 const countdownEl = document.querySelector("#countdown");
 
 setInterval(updateCountdown, 1000);
 
 function updateCountdown() {
-  const minutes = Math.floor(time / 60);
+  const hours = Math.floor(time / 3600);
+  let minutes = Math.floor(time / 60 % 60);
   let seconds = time % 60;
 
+  minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  countdownEl.innerHTML = `${minutes}:${seconds}`;
+  countdownEl.innerHTML = `${hours}:${minutes}:${seconds}`;
   time--;
 }
